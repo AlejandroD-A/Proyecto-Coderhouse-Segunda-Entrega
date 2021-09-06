683f446c107a0acaad818bebd2f7c6429c328f76
Alejandro <alejandrodaliaga@gmail.com>
2021-09-06 18:34:00 -0300

    Se cambian las variables en ingles

--- src/persistence/mongo/schemas/ProductSchema.js
+++ src/persistence/mongo/schemas/ProductSchema.js
@@ -4,11 +4,11 @@ const Cart = require('./CartSchema')
 
 const ProductSchema = new mongoose.Schema({
     timestamp : {type: Date, default : new Date(), required: true },
-    nombre: { type: String, required: true },
-    descripcion:{ type: String, required: true},
-    codigo: { type: String, required: true },
-    foto: { type: String, required: true },
-    precio: { type: Number, required: true},
+    title: { type: String, required: true },
+    description:{ type: String, required: true},
+    code: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    price: { type: Number, required: true},
     stock: { type: Number, required: true, default: 0 }    
 })
 
