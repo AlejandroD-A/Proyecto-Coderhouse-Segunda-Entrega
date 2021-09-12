53baf7a97a3bd0c0f4a619d54114313d0039ac6a
Alejandro <alejandrodaliaga@gmail.com>
2021-09-12 16:28:19 -0300

    Evita cargar recursos si no esta loggeado

--- client/src/components/BtnAddToCart/index.js
+++ client/src/components/BtnAddToCart/index.js
@@ -1,14 +1,18 @@
-import { useState, useCallback } from 'react'
+import { useCallback } from 'react'
 
 import useCart from 'hooks/useCart'
+import useUser from 'hooks/useUser'
 
 import { IoBagAddOutline, IoBagAdd } from 'react-icons/io5'
 
 function BtnAddToCart({idProduct}) {
+
     const { cartItems, addNew, remove } = useCart()
+    const { isLogged } = useUser()
 
     const isAdded = useCallback(
         () => {
+            if(!isLogged) return false
             let itemCart = cartItems.find( (cartProduct)=> cartProduct.product._id == idProduct )
             return itemCart
         }   
@@ -16,7 +20,7 @@ function BtnAddToCart({idProduct}) {
     )
 
 	const handleButton = () => {
-
+        if(!isLogged ) return alert('Debe Iniciar Sesion ')
         const item = isAdded()
         if( !item ){
             addNew(idProduct)
