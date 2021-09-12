f8d9c46f1dd08ba180f693ca3984645b7942510a
Alejandro <alejandrodaliaga@gmail.com>
2021-09-12 14:13:18 -0300

    Agrega ruta user para obtener usuario autenticado

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const checkAuth = require('../middlewares/checkAuth')
 
-router.post('/signup', passport.authenticate('signup' ),async (req,res)=>{
+router.post('/signup', passport.authenticate('signup' ), async (req,res) => {
     res.json({ message:"Success", user: req.user })
 })
 
-router.post('/login', passport.authenticate('login' ),async (req,res)=>{
+router.post('/login', passport.authenticate('login' ), async (req,res) => {
+    res.json({ message:"Success", user: req.user })
+})
+
+router.get('/user', checkAuth, async (req,res)=>{
     res.json({ message:"Success", user: req.user })
 })
 
