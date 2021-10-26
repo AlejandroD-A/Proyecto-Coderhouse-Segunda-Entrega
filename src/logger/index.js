1b574b1704ea96b4548be9d1d1f316f0036222c1
Alejandro <alejandrodaliaga@gmail.com>
2021-10-26 20:25:38 -0300

    Cambia logger para utilizar multiples files

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,7 +1,37 @@
 const winston = require ('winston')
 
-
 const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({ 
+            level: 'info', 
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+              )
+            })
+        ]
+})
+
+const loggerError = winston.createLogger({
+    transports: [
+        new winston.transports.Console({ 
+            level: 'info', 
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+              )
+            }),
+        new winston.transports.File({
+             filename: __dirname + '/logs/error.log', 
+             level: 'error', 
+             format:  winston.format.combine(
+                winston.format.timestamp(), 
+                winston.format.json()
+             )})
+    ]
+})
+
+const loggerWarn = winston.createLogger({
     transports: [
         new winston.transports.Console({ 
             level: 'info', 
@@ -17,10 +47,13 @@ const logger = winston.createLogger({
                winston.format.timestamp(), 
                winston.format.json()
             )
-           })
+           }),
     ]
 })
 
 
-
-module.exports = logger
+module.exports = { 
+    info:(msg)=> logger.info(msg),
+    warn:(msg)=> loggerWarn.warn(msg),
+    error:(msg)=> loggerError.error(msg)
+}
