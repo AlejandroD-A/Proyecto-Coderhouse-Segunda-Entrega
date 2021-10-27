4be7ee479c8bd014d90d46567cde4b712d6d49f6
Alejandro <alejandrodaliaga@gmail.com>
2021-10-27 19:21:08 -0300

    Cambia funciones de modulo a instancia de Clase

--- src/messaging/mail.js
+++ src/messaging/mail.js
@@ -1,7 +1,8 @@
 const nodemailer = require('nodemailer')
 const logger = require('../logger')
 const config = require('../config')
-const ejs = require('ejs')
+const ejs = require('ejs');
+const { NetworkContext } = require('twilio/lib/rest/supersim/v1/network');
 
 const GmailTransporter = nodemailer.createTransport({
     service: 'gmail',
@@ -22,70 +23,77 @@ const MailtrapTransporter = nodemailer.createTransport({
 
 const transporter = config.NODE_ENV == 'development' ? MailtrapTransporter : GmailTransporter
 
-const newRegister = async (data) =>{
-    try{ 
-        await transporter.sendMail({
-            from: 'Ecommerce Ale',
-            to: config.GMAIL_USER,
-            subject: `Nuevo registro ${data.email}`,
-            html: ` <p>${data.name}</p> 
-                    <p>${data.lastName}</p> 
-                    <p>${data.email}</p> 
-                    <p>${data.age}</p>
-                    <p>${data.phone}</p>
-                    <img src=${data.avatar}/>`,
-        })
-    }catch(err){
-        logger.error(`Ha ocurrido un error al enviar el mail de registro ${err.message}, usuario: ${data.email}`)
-    }
-}
 
-const newOrder = async  ( user , products) => {
-    try{
-        await transporter.sendMail({
-            from: 'Ecommerce Ale',
-            to: config.GMAIL_USER,
-            subject: `Nuevo Pedido de ${user.name} - ${user.email}`,
-            html: `
-                <h5> Usuario: </h5>
-                    <p>${user.name} ${user.lastName}</p> 
-                    <p>${user.email}</p> 
-                    <p>${user.phone}</p>
-                    </hr>
-        
-                    <h2> Pedido </h2>
-                    <ul>
-                     ${ products.map(product => `<li>${product.title}</li>`) } 
-                    </ul>
-                    `
-        })
-    }catch(err){
-        logger.error(`Ha ocurrido un error al enviar un mail ${err.message}, usuario: ${user.email}`)
+class MailSender{
+    constructor(transporter){
+        this.transporter = transporter
     }
+
+    async newRegister(data){
+        try{ 
+            await this.transporter.sendMail({
+                from: 'Ecommerce Ale',
+                to: config.GMAIL_USER,
+                subject: `Nuevo registro ${data.email}`,
+                html: ` <p>${data.name}</p> 
+                        <p>${data.lastName}</p> 
+                        <p>${data.email}</p> 
+                        <p>${data.age}</p>
+                        <p>${data.phone}</p>
+                        <img src=${data.avatar}/>`,
+            })
+        }catch(err){
+            logger.error(`Ha ocurrido un error al enviar el mail de registro ${err.message}, usuario: ${data.email}`)
+        }
+    }
+
+    async newOrder( user, products ){
+        try{
+            await this.transporter.sendMail({
+                from: 'Ecommerce Ale',
+                to: config.GMAIL_USER,
+                subject: `Nuevo Pedido de ${user.name} - ${user.email}`,
+                html: `
+                    <h5> Usuario: </h5>
+                        <p>${user.name} ${user.lastName}</p> 
+                        <p>${user.email}</p> 
+                        <p>${user.phone}</p>
+                        </hr>
+            
+                        <h2> Pedido </h2>
+                        <ul>
+                         ${ products.map(product => `<li>${product.title}</li>`) } 
+                        </ul>
+                        `
+            })
+        }catch(err){
+            logger.error(`Ha ocurrido un error al enviar un mail ${err.message}, usuario: ${user.email}`)
+        }
+    }
+
+    async sendEmailError(err){
+        try{
+            let html = ""
+            ejs.renderFile(__dirname+'/templates/errorTemplate.ejs', { error: err },{},(err,str) => {
+                if(err) throw err
+                html = str
+            });
     
-}
-
-const sendEmailError = async (err) =>{
-    try{
-        let html = ""
-        ejs.renderFile(__dirname+'/templates/errorTemplate.ejs', { error: err },{},(err,str) => {
-            if(err) throw err
-            html = str
-        });
-        console.log(err)
-
-        await transporter.sendMail({
-            from: 'Ecommerce Ale',
-            to: config.GMAIL_USER,
-            subject: `Ha Ocurrido un Error`,
-            html: html
-        })
-
-        logger.info(`Se ha enviado el mail de Error`)
-    }catch(err){
-        console.log(err)
-        logger.error(err.message)
+            await this.transporter.sendMail({
+                from: 'Ecommerce Ale',
+                to: config.GMAIL_USER,
+                subject: `Ha Ocurrido un Error`,
+                html: html
+            })
+    
+            return logger.info(`Se ha enviado el mail de Error`)
+        }catch(err){
+            return logger.error(err.message)
+             
+        }
     }
+
 }
 
-module.exports = { newRegister, newOrder, sendEmailError }
+
+module.exports = new MailSender(transporter)
