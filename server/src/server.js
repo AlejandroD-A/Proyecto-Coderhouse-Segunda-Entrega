b34151cf2f1bd691fe4072568ca07781082f9467
Alejandro <alejandrodaliaga@gmail.com>
2021-11-03 19:40:43 -0300

    Agrega permisos de Cors a vercel DevDeploy

--- server/src/server.js
+++ server/src/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({
 }))
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000','https://proyecto-coderhouse-trabajo-final-4z3mmc3do-alejandrod-a.vercel.app'],
 }))
 
 // Static Files 
