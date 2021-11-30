617abd830057174333a27931f747a38e9c4f7495
Alejandro <alejandrodaliaga@gmail.com>
2021-11-30 20:32:32 -0300

    Fix:  no mostraba correctamente los errores

--- server/src/middlewares/handleErrors.js
+++ server/src/middlewares/handleErrors.js
@@ -7,7 +7,7 @@ module.exports =  async (error, req, res, next) => {
         if(error.name == "ValidationError"){
             return res.status(422).json({ code: 422, message: error.message });
         }
-        logger.error(error)
+        logger.error(error.message, error)
         await mail.sendEmailError(error)
         return res.status(500).json({ error : 'Ha ocurrido un Error' })
     }catch(err){
